08fec4dc6c1849092c07e33362e3a852ac670cc2
starkk242 <bimalsingh983@gmail.com>
2024-05-26 01:43:11 +0530

    Git Ignore Changes and Tests

--- rentals/tests.py
+++ rentals/tests.py
@@ -1,3 +1,14 @@
+# rentals/tests.py
 from django.test import TestCase
+from .models import Book, Rental
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class RentalTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser')
+        self.book = Book.objects.create(title='Test Book', author='Author', number_of_pages=300)
+        self.rental = Rental.objects.create(user=self.user, book=self.book)
+
+    def test_calculate_fee(self):
+        self.rental.end_date = self.rental.start_date + timedelta(days=60)
+        self.assertEqual(self.rental.calculate_fee(), 30 * (self.book.number_of_pages / 100))
